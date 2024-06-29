cb4ebf92b57535ba6b5b818c5d4a3e2ead1236d4
usemrlin <lin950821@163.com>
2024-06-29 15:51:24 +0800

    项目搭建完毕

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'antd-mobile'
+const App = () => {
+  return <div>
+    <Button color="success">点击</Button>
+  </div>
+}
+export default App
---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,5 @@
+const detail = () => {
+    return <div>详情</div>
+}
+
+export default detail
